adf35c4b9d465c04b5f3e8eb542780a7557fd9c6
Dawson Hessler <funkemunkybiz@gmail.com>
2019-07-13 14:29:37 -0400

    Fixed event bug in Kauri with FunkeListeners
    This is very important since it potentially fixes many false positives in checks like NoSlowdown (Type A) since this event is used to update the ActionProcessor isUsingItem feature.

--- Kauri/Impl/src/main/java/cc/funkemunky/anticheat/impl/listeners/FunkeListeners.java
+++ Kauri/Impl/src/main/java/cc/funkemunky/anticheat/impl/listeners/FunkeListeners.java
@@ -5,13 +5,14 @@ import cc.funkemunky.anticheat.api.data.PlayerData;
 import cc.funkemunky.api.Atlas;
 import cc.funkemunky.api.events.AtlasListener;
 import cc.funkemunky.api.events.Listen;
+import cc.funkemunky.api.events.ListenerPriority;
 import cc.funkemunky.api.events.impl.TickEvent;
 import cc.funkemunky.api.utils.Init;
 
 @Init
 public class FunkeListeners implements AtlasListener {
 
-    @Listen
+    @Listen(priority = ListenerPriority.HIGH)
     public void onTickEvent(TickEvent event) {
         Atlas.getInstance().executeTask(() -> Kauri.getInstance().getDataManager().getDataObjects().keySet().forEach(key -> {
             PlayerData data = Kauri.getInstance().getDataManager().getDataObjects().get(key);
