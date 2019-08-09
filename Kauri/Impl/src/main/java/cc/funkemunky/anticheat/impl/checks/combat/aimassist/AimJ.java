a98118b76f7521011808af7dc9f570f989febfb4
funkemunky <funkemunkybiz@gmail.com>
2019-08-09 19:15:16 -0400

    shit

--- Kauri/Impl/src/main/java/cc/funkemunky/anticheat/impl/checks/combat/aimassist/AimJ.java
+++ Kauri/Impl/src/main/java/cc/funkemunky/anticheat/impl/checks/combat/aimassist/AimJ.java
@@ -25,20 +25,26 @@ public class AimJ extends Check {
         if(move.getYawDelta() == 0) return;
 
         float offset = (move.getYawGCD() / (float) move.getOffset());
+        float delta = MathUtils.getDelta(move.getYawDelta(), move.getLastYawDelta()) / offset % 1;
         if(move.getYawDelta() != move.getLastYawDelta()
                 && move.getYawDelta() < 12
                 && move.getLastYawDelta() < 12
                 && offset < 0.05
                 && !getData().isCinematicMode()
-                && MathUtils.getDelta(move.getYawDelta(), move.getLastYawDelta()) < 2
-                && move.getPitchDelta() < 3
-                && (MathUtils.getDelta(move.getYawDelta(), move.getLastYawDelta()) / offset % 1 > 0.1)
+                && MathUtils.getDelta(move.getYawDelta(), move.getLastYawDelta()) < 4
+                && move.getPitchDelta() < 5
+                && (delta > 0.02)
                 && move.getYawGCD() == move.getLastYawGCD()) {
-            if(vl++ > 3) {
-                flag("g=" + move.getYawGCD() + " y1=" + move.getYawDelta() + " y2=" + move.getLastYawDelta(), true, true, AlertTier.HIGH);
-            }
-            debug(Color.Green + "Flag: " + "yaw=" + move.getYawDelta() + " lastYaw=" + move.getLastYawDelta() + " gcd=" + (move.getYawGCD() / (float) move.getOffset()) + " vl=" + vl);
+            //if(vl++ > 3) {
+               // flag("g=" + move.getYawGCD() + " y1=" + move.getYawDelta() + " y2=" + move.getLastYawDelta(), true, true, AlertTier.HIGH);
+            //}
+            //debug(Color.Green + "Flag: " + "yaw=" + move.getYawDelta() + " lastYaw=" + move.getLastYawDelta() + " gcd=" + (move.getYawGCD() / (float) move.getOffset()) + " vl=" + vl);
         } else vl -= vl > 0 ? 0.04 : 0;
+
+        if(move.getYawDelta() != move.getLastYawDelta()
+                && move.getYawGCD() == move.getLastYawGCD()) {
+            debug("delta" + delta + " offset=" + offset);
+        }
     }
 
     @Override
