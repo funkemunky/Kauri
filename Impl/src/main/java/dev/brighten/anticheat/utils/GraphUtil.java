938d4357bfd52caba6d3872f9ab44b939e9fa89a
ElevatedDev <45542136+ElevatedDev@users.noreply.github.com>
2020-01-15 18:50:13 +0200

    Great AutoClicker check.

--- Impl/src/main/java/dev/brighten/anticheat/utils/GraphUtil.java
+++ Impl/src/main/java/dev/brighten/anticheat/utils/GraphUtil.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.bukkit.ChatColor;
 
+import java.util.Deque;
 import java.util.List;
 
 public class GraphUtil {
@@ -18,6 +19,40 @@ public class GraphUtil {
     }
 
     public static GraphResult getGraph(List<Float> values) {
+        StringBuilder graph = new StringBuilder();
+
+        double largest = 0;
+
+        for (double value : values) {
+            if (value > largest)
+                largest = value;
+        }
+
+        int GRAPH_HEIGHT = 2;
+        int positives = 0, negatives = 0;
+
+        for (int i = GRAPH_HEIGHT - 1; i > 0; i -= 1) {
+            StringBuilder sb = new StringBuilder();
+
+            for (double index : values) {
+                double value = GRAPH_HEIGHT * index / largest;
+
+                if (value > i && value < i + 1) {
+                    ++positives;
+                    sb.append(String.format("%s+", ChatColor.GREEN));
+                } else {
+                    ++negatives;
+                    sb.append(String.format("%s-", ChatColor.RED));
+                }
+            }
+
+            graph.append(sb.toString());
+        }
+
+        return new GraphResult(graph.toString(), positives, negatives);
+    }
+
+    public static GraphResult getGraphLong(Deque<Long> values) {
         StringBuilder graph = new StringBuilder();
 
         double largest = 0;
