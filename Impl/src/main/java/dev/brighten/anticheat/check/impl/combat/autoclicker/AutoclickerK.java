2b9e5d1eb7e0f43d3b269bc35301abdd7199b91e
ElevatedDev <45542136+ElevatedDev@users.noreply.github.com>
2020-01-23 14:19:39 +0200

    The autoclicker check.

---
+++ Impl/src/main/java/dev/brighten/anticheat/check/impl/combat/autoclicker/AutoclickerK.java
@@ -0,0 +1,50 @@
+package dev.brighten.anticheat.check.impl.combat.autoclicker;
+
+import dev.brighten.anticheat.check.api.Check;
+import dev.brighten.anticheat.check.api.CheckInfo;
+import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInArmAnimationPacket;
+import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInBlockDigPacket;
+import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInFlyingPacket;
+import dev.brighten.anticheat.check.api.*;
+import dev.brighten.api.check.CheckType;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+@CheckInfo(name = "Autoclicker (K)", description = "STD Autoclicker check (fast)", checkType = CheckType.AUTOCLICKER)
+public class AutoclickerK extends Check {
+
+    private Deque<Long> clickSamples = new LinkedList<>();
+
+    private long lastSwing;
+
+    @Packet
+    public void onClick(WrappedInArmAnimationPacket packet, long timeStamp) {
+        long now = System.currentTimeMillis();
+        long delay = now - this.lastSwing;
+
+        if (data.playerInfo.lastBrokenBlock.hasNotPassed(3) || data.playerInfo.lastBlockPlace.hasNotPassed(1))
+            return;
+
+        if (delay > 1L && delay < 300L && this.clickSamples.add(delay) && this.clickSamples.size() == 30) {
+            double average = this.clickSamples.stream().mapToDouble(Long::doubleValue).average().orElse(0.0);
+
+            double stdDeviation = 0.0;
+
+            for (Long click : this.clickSamples) {
+                stdDeviation += Math.pow(click.doubleValue() - average, 2);
+            }
+
+            stdDeviation /= this.clickSamples.size();
+
+            if (Math.sqrt(stdDeviation) < 30.d) {
+                vl++;
+                this.flag("STD: " + stdDeviation);
+            }
+
+            this.clickSamples.clear();
+        }
+
+        this.lastSwing = now;
+    }
+}
