766ad8ef14e940418ffa0909869e2aac6e435b4c
Dawson Hessler <funkemunkybiz@gmail.com>
2020-08-02 14:17:05 -0400

    Update Hitboxes.java

--- Impl/src/main/java/dev/brighten/anticheat/check/impl/combat/hitbox/Hitboxes.java
+++ Impl/src/main/java/dev/brighten/anticheat/check/impl/combat/hitbox/Hitboxes.java
@@ -10,6 +10,7 @@ import cc.funkemunky.api.utils.world.types.SimpleCollisionBox;
 import dev.brighten.anticheat.check.api.*;
 import dev.brighten.api.check.CheckType;
 import lombok.val;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
 public class Hitboxes extends Check {
 
     private boolean useEntity;
+    private float buffer;
     private Entity target;
 
     @Packet
@@ -40,6 +42,10 @@ public class Hitboxes extends Check {
                         .equals(packet.getPlayer().getLocation().getWorld().getUID()))
                     break check;
 
+                if(data.pastLocation.previousLocations.size() < 10
+                        || data.getPlayer().getGameMode().equals(GameMode.CREATIVE)
+                        || data.getPlayer().getGameMode().equals(GameMode.SPECTATOR)) break check;
+
                 double distance = target.getLocation().distance(packet.getPlayer().getLocation());
                 double vExpand = Math.abs(data.playerInfo.deltaPitch / 90) * distance;
                 double hExpand = Math.abs(data.playerInfo.deltaYaw / 180) * distance;
@@ -48,15 +54,19 @@ public class Hitboxes extends Check {
                         .add(0, packet.getPlayer().isSneaking() ? 1.54 : 1.62, 0);
 
                 RayCollision ray = new RayCollision(origin.toVector(), origin.getDirection());
-                boolean collided = data.targetPastLocation.getEstimatedLocation(now, data.lagInfo.transPing, 100L)
+                boolean collided = data.targetPastLocation.getEstimatedLocation(now, data.lagInfo.transPing, 150L)
                         .stream().map(loc -> ((SimpleCollisionBox)EntityData.getEntityBox(loc, target))
                                 .expand(0.1).expand(hExpand, vExpand, hExpand))
                 .anyMatch(ray::isCollided);
 
                 if(!collided) {
-                    vl++;
-                    flag("v=%v.3 h=%v.3 ping=%v", vExpand, hExpand, data.lagInfo.transPing);
-                }
+                    if(++buffer > 2) {
+                        vl++;
+                        flag("v=%v.3 h=%v.3 ping=%v", vExpand, hExpand, data.lagInfo.transPing);
+                    }
+                } else if(buffer > 0) buffer-= 0.2;
+
+                debug("buffer=%v.1 collided=%v", buffer, collided);
             }
             useEntity = false;
         }
