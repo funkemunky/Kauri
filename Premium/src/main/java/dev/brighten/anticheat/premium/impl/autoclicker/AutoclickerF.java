d6f9cd9c2cb32d57d597881a21b2c196d7c17f80
Dawson Hessler <funkemunkybiz@gmail.com>
2020-08-02 12:40:50 -0400

    Revert "Started with check"
    This reverts commit 901d216f1f257f80b6207e532787b00483fa7735.

--- Premium/src/main/java/dev/brighten/anticheat/premium/impl/autoclicker/AutoclickerF.java
+++ Premium/src/main/java/dev/brighten/anticheat/premium/impl/autoclicker/AutoclickerF.java
@@ -3,36 +3,49 @@ package dev.brighten.anticheat.premium.impl.autoclicker;
 import cc.funkemunky.api.tinyprotocol.api.ProtocolVersion;
 import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInArmAnimationPacket;
 import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInFlyingPacket;
-import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInUseEntityPacket;
 import dev.brighten.anticheat.check.api.Check;
 import dev.brighten.anticheat.check.api.CheckInfo;
 import dev.brighten.anticheat.check.api.Packet;
+import dev.brighten.anticheat.utils.MiscUtils;
 import dev.brighten.api.check.CheckType;
+import lombok.val;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CheckInfo(name = "Autoclicker (F)", description = "Checks for a constant skew of values.", developer = true,
         checkType = CheckType.AUTOCLICKER, maxVersion = ProtocolVersion.V1_8_9)
 public class AutoclickerF extends Check {
 
-    public int flying, buffer, ticks;
+    public int flying, buffer;
+
+    public List<Float> cpsList = new ArrayList<>();
     @Packet
     public void onFlying(WrappedInFlyingPacket packet) {
         flying++;
     }
 
     @Packet
-    public void onUse(WrappedInUseEntityPacket packet) {
-        ticks = 0;
-    }
-
-    @Packet
     public void onArm(WrappedInArmAnimationPacket packet) {
-        if(data.playerInfo.breakingBlock)
-            ticks = 0;
+        float cps = 20.f / flying;
 
-        if(flying > 3) ticks = 0;
-        else ticks++;
+        cpsList.add(cps);
 
-        debug("flying=%v ticks=%v", flying, ticks);
+        if(cpsList.size() == 20) {
+            val skewness = MiscUtils.getSkewness(cpsList);
+            val variance = MiscUtils.stdev(cpsList);
+
+            if(variance > 2.5 && Math.abs(skewness) < 0.1) {
+                buffer+= 4;
+                if(buffer > 6) {
+                    vl++;
+                    flag("skew=%v.2 variance=%v.3", skewness, variance);
+                }
+            } else if(buffer > 0) buffer--;
+
+            debug("skew=%v.4 std=%v.4 buffer=%v", skewness, variance, buffer);
+            cpsList.clear();
+        }
         flying = 0;
     }
 }
