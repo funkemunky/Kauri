0b9b3ab5f42ad7a8dac9ba6526288c2b68b46b5e
Dawson Hessler <funkemunkybiz@gmail.com>
2020-08-15 11:15:44 -0400

    Updated reach check to be more accurate and flag more

--- Premium/src/main/java/dev/brighten/anticheat/premium/impl/hitboxes/ReachB.java
+++ Premium/src/main/java/dev/brighten/anticheat/premium/impl/hitboxes/ReachB.java
@@ -13,10 +13,14 @@ import cc.funkemunky.api.utils.world.types.RayCollision;
 import cc.funkemunky.api.utils.world.types.SimpleCollisionBox;
 import dev.brighten.anticheat.Kauri;
 import dev.brighten.anticheat.check.api.*;
+import dev.brighten.anticheat.utils.AxisAlignedBB;
+import dev.brighten.anticheat.utils.Vec3D;
 import dev.brighten.api.check.CheckType;
 import lombok.val;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -51,26 +55,42 @@ public class ReachB extends Check {
 
             entityLocs.clear();
 
-            double distance = 69, horzDistance = 69;
-            int misses = 0, collided = 0;
-            for (KLocation originLoc : Arrays.asList(data.playerInfo.to.clone(), data.playerInfo.from.clone())) {
-                originLoc.y+= data.playerInfo.sneaking ? 1.54 : 1.62;
-                RayCollision ray = new RayCollision(originLoc.toVector(), MathUtils.getDirection(originLoc));
-                if(debug) ray.draw(WrappedEnumParticle.CRIT, Bukkit.getOnlinePlayers());
-                for (SimpleCollisionBox sbox : simpleBoxes) {
-                    SimpleCollisionBox box = sbox.copy();
-                    box.expand(0.1);
-                    if(debug) box.draw(WrappedEnumParticle.FLAME, Bukkit.getOnlinePlayers());
-                    val check = RayCollision.distance(ray, box);
+            double distance = 69, fdistance = 69, tdistance = 69;
+            int misses = 0, collided = 0, fmisses = 0, tmisses = 0, fcollided = 0, tcollided = 0;
+            Location toOrigin = data.playerInfo.to.toLocation(data.getPlayer().getWorld()),
+                    fromOrigin = data.getPlayer().getEyeLocation();
 
-                    horzDistance = Math.min(horzDistance, box.max().midpoint(box.min()).setY(0)
-                            .distance(originLoc.toVector().setY(0)) - .4);
-                    if(check == -1) {
-                        misses++;
-                        continue;
-                    } else collided++;
-                    distance = Math.min(distance, check);
-                }
+            toOrigin.setY(toOrigin.getY() + (data.playerInfo.sneaking ? 1.54 : 1.62));
+            for (SimpleCollisionBox sbox : simpleBoxes) {
+                val copied = sbox.copy().expand(0.1);
+                AxisAlignedBB aabb = new AxisAlignedBB(copied);
+                if(debug) copied.draw(WrappedEnumParticle.FLAME, Bukkit.getOnlinePlayers());
+                Vec3D checkTo = aabb.rayTrace(toOrigin.toVector(), toOrigin.getDirection(), 10),
+                checkFrom = aabb.rayTrace(fromOrigin.toVector(), fromOrigin.getDirection(), 10);
+
+                if(checkTo != null) {
+                    tdistance = Math.min(new Vector(checkTo.x, checkTo.y, checkTo.z)
+                            .distance(toOrigin.toVector()), tdistance);
+                    tcollided++;
+                } else tmisses++;
+
+                if(checkFrom != null) {
+                    fdistance = Math.min(new Vector(checkFrom.x, checkFrom.y, checkFrom.z)
+                            .distance(fromOrigin.toVector()), fdistance);
+                    fcollided++;
+                } else fmisses++;
+            }
+
+            boolean usedFrom = false;
+            if(fmisses <= tmisses) {
+                misses = fmisses;
+                collided = fcollided;
+                distance = fdistance;
+                usedFrom = true;
+            } else {
+                misses = tmisses;
+                collided = tcollided;
+                distance = tdistance;
             }
 
             if(distance == 69) {
@@ -84,13 +104,14 @@ public class ReachB extends Check {
                         Kauri.INSTANCE.lastTickLag.hasPassed(40)) {
                     if(++buffer > 4) {
                         vl++;
-                        flag("distance=%v.3 buffer=%v.1 misses=%v", distance, buffer, misses);
+                        flag("distance=%v.3 from=%v buffer=%v.1 misses=%v",
+                                distance, usedFrom, buffer, misses);
                     }
                 } else buffer-= buffer > 0 ? data.playerVersion.isAbove(ProtocolVersion.V1_8_9) ? 0.1f : 0.05f : 0;
             }
 
-            debug("distance=%v.3 hdist=%v.3 buffer=%v.2 ticklag=%v collided=%v delta=%v",
-                    distance, horzDistance, buffer, Kauri.INSTANCE.lastTickLag.getPassed(), collided,
+            debug("distance=%v.3 from=%v buffer=%v.2 ticklag=%v collided=%v delta=%v",
+                    distance, usedFrom, buffer, Kauri.INSTANCE.lastTickLag.getPassed(), collided,
                     timeStamp - lastUse);
         }
     }
