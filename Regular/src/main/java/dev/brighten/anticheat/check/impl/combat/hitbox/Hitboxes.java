6e316ce7418dcbc5e212437ea9488bc9f92b1300
Dawson Hessler <funkemunkybiz@gmail.com>
2020-08-26 15:42:37 -0400

    Update Hitboxes.java

--- Regular/src/main/java/dev/brighten/anticheat/check/impl/combat/hitbox/Hitboxes.java
+++ Regular/src/main/java/dev/brighten/anticheat/check/impl/combat/hitbox/Hitboxes.java
@@ -1,118 +1,75 @@
 package dev.brighten.anticheat.check.impl.combat.hitbox;
 
+import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInFlyingPacket;
 import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInUseEntityPacket;
 import cc.funkemunky.api.utils.KLocation;
 import cc.funkemunky.api.utils.MathUtils;
-import cc.funkemunky.api.utils.world.CollisionBox;
 import cc.funkemunky.api.utils.world.EntityData;
-import cc.funkemunky.api.utils.world.types.ComplexCollisionBox;
 import cc.funkemunky.api.utils.world.types.RayCollision;
 import cc.funkemunky.api.utils.world.types.SimpleCollisionBox;
-import dev.brighten.anticheat.Kauri;
 import dev.brighten.anticheat.check.api.*;
-import dev.brighten.anticheat.data.ObjectData;
-import dev.brighten.anticheat.utils.AtomicDouble;
 import dev.brighten.api.check.CheckType;
+import lombok.val;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @CheckInfo(name = "Hitboxes", description = "Checks if the player attacks outside a player's hitbox.",
-        checkType = CheckType.HITBOX, punishVL = 15)
+        checkType = CheckType.HITBOX, punishVL = 15, developer = true)
 @Cancellable(cancelType = CancelType.ATTACK)
 public class Hitboxes extends Check {
 
-    private static List<EntityType> allowedEntities = Arrays.asList(
-            EntityType.ZOMBIE,
-            EntityType.VILLAGER,
-            EntityType.PLAYER,
-            EntityType.SKELETON,
-            EntityType.PIG_ZOMBIE,
-            EntityType.WITCH,
-            EntityType.CREEPER,
-            EntityType.ENDERMAN);
-
-    @Setting(name = "allowNPCFlag")
-    private static boolean allowNPCFlag = true;
+    private boolean useEntity;
+    private float buffer;
+    private Entity target;
 
     @Packet
-    public void onFlying(WrappedInUseEntityPacket packet, long timeStamp) {
-        if (checkParameters(data)) {
-
-            List<RayCollision> rayTrace = Stream.of(data.playerInfo.to.clone(), data.playerInfo.from.clone())
-                    .map(l -> {
-                        KLocation loc = l.clone();
-                        loc.y+=data.playerInfo.sneaking ? 1.54f : 1.62f;
-                        return new RayCollision(loc.toVector(),
-                                MathUtils.getDirection(loc));
-                    })
-                    .collect(Collectors.toList());
-
-            List<SimpleCollisionBox> entityLocations = new ArrayList<>();
-
-
-           data.targetPastLocation
-                    .getEstimatedLocation(data.lagInfo.transPing * 50, 150
-                                    + Math.abs(data.lagInfo.transPing - data.lagInfo.lastTransPing))
-                    .stream()
-                    .map(loc -> getHitbox(loc, data.target))
-                    .forEach(box -> box.downCast(entityLocations));
-
-            long collisions = 0;
-            AtomicDouble distance = new AtomicDouble(10);
-
-            for (RayCollision ray : rayTrace) {
-                collisions+= entityLocations.stream().filter(bb -> {
-                    Vector point;
-                    if((point = ray.collisionPoint(bb)) != null) {
-                        double dist = point.distance(ray.getOrigin());
-
-                        distance.set(Math.min(dist, distance.get()));
-                        return dist < 3.65f;
-                    }
-                    return false;
-                }).count();
-            }
-
-            if (collisions == 0
-                    && timeStamp - data.creation > 3000L
-                    && data.lagInfo.lastPingDrop.hasPassed(10)
-                    && data.lagInfo.lastPacketDrop.hasPassed(4)) {
-                if(vl++ > 10)  flag("distance=%v ping=%p tps=%t",
-                        distance.get() != -1 ? distance.get() : "[none collided]");
-            } else vl -= vl > 0 ? 0.25 : 0;
-
-            debug("collided=" + collisions + " distance=" + distance.get() + " type=" + data.target.getType());
+    public void onUse(WrappedInUseEntityPacket packet) {
+        if (packet.getAction().equals(WrappedInUseEntityPacket.EnumEntityUseAction.ATTACK)) {
+            useEntity = true;
+            target = packet.getEntity();
         }
     }
 
-    private static boolean checkParameters(ObjectData data) {
-        return data.playerInfo.lastAttack.hasNotPassed(0)
-                && data.target != null
-                && data.targetPastLocation.previousLocations.size() > 12
-                && Kauri.INSTANCE.lastTickLag.hasPassed(10)
-                && allowedEntities.contains(data.target.getType())
-                && !data.playerInfo.creative
-                && data.playerInfo.lastTargetSwitch.hasPassed()
-                && !data.getPlayer().getGameMode().equals(GameMode.CREATIVE);
+    @Packet
+    public void onFlying(WrappedInFlyingPacket packet, long now) {
+        if (useEntity) {
+            check:
+            {
+                if(!target.getLocation().getWorld().getUID()
+                        .equals(packet.getPlayer().getLocation().getWorld().getUID()))
+                    break check;
+
+                if(data.pastLocation.previousLocations.size() < 10
+                        || data.getPlayer().getGameMode().equals(GameMode.CREATIVE)
+                        || data.getPlayer().getGameMode().equals(GameMode.SPECTATOR)) break check;
+
+                double distance = target.getLocation().distance(packet.getPlayer().getLocation());
+                double vExpand = Math.abs(data.playerInfo.deltaPitch / 90) * distance;
+                double hExpand = Math.abs(data.playerInfo.deltaYaw / 180) * distance;
+
+                Location origin = packet.getPlayer().getLocation().clone()
+                        .add(0, packet.getPlayer().isSneaking() ? 1.54 : 1.62, 0);
+
+                RayCollision ray = new RayCollision(origin.toVector(), origin.getDirection());
+                boolean collided = data.targetPastLocation.getEstimatedLocation(now, data.lagInfo.transPing, 150L)
+                        .stream().map(loc -> ((SimpleCollisionBox)EntityData.getEntityBox(loc, target))
+                                .expand(0.1).expand(hExpand, vExpand, hExpand))
+                        .anyMatch(ray::isCollided);
+
+                if(!collided) {
+                    if(++buffer > 2) {
+                        vl++;
+                        flag("v=%v.3 h=%v.3 ping=%v", vExpand, hExpand, data.lagInfo.transPing);
+                    }
+                } else if(buffer > 0) buffer-= 0.2;
+
+                debug("buffer=%v.1 collided=%v", buffer, collided);
+            }
+            useEntity = false;
+        }
     }
 
-    private static CollisionBox getHitbox(KLocation loc, Entity type) {
-        List<SimpleCollisionBox> boxes = new ArrayList<>();
-
-        EntityData.getEntityBox(loc, type).downCast(boxes);
-
-        boxes.forEach(box -> box.expand(0.2));
-
-        if(boxes.size() == 1) return boxes.get(0);
-
-        return new ComplexCollisionBox(boxes.toArray(new SimpleCollisionBox[0]));
-    }
 }
