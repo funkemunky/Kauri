f010c9f3ea6737d9d8dce18b07cea8478fc19586
Dawson Hessler <funkemunkybiz@gmail.com>
2020-08-28 11:17:18 -0400

    Create SpeedD.java

---
+++ Premium/src/main/java/dev/brighten/anticheat/premium/impl/SpeedD.java
@@ -0,0 +1,21 @@
+package dev.brighten.anticheat.premium.impl;
+
+import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInFlyingPacket;
+import dev.brighten.anticheat.check.api.Check;
+import dev.brighten.anticheat.check.api.CheckInfo;
+import dev.brighten.anticheat.check.api.Packet;
+import dev.brighten.api.check.CheckType;
+
+@CheckInfo(name = "Speed (D)", description = "Minecraft code acceleration check.",
+        checkType = CheckType.SPEED, developer = true)
+public class SpeedD extends Check {
+
+    @Packet
+    public void onFlying(WrappedInFlyingPacket packet) {
+        if(!packet.isPos()
+                || (data.playerInfo.deltaY == 0 || data.playerInfo.deltaXZ == 0)
+                || data.playerInfo.serverPos) return;
+
+
+    }
+}
