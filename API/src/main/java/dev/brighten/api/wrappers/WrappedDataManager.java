f4de41ca6ee7bfe386cbc89f09b242f1596fac57
Dawson Hessler <funkemunkybiz@gmail.com>
2020-11-26 12:48:47 -0500

    Fixing up the API so everything compiles properly

--- API/src/main/java/dev/brighten/api/wrappers/WrappedDataManager.java
+++ API/src/main/java/dev/brighten/api/wrappers/WrappedDataManager.java
@@ -2,22 +2,34 @@ package dev.brighten.api.wrappers;
 
 import cc.funkemunky.api.reflections.Reflections;
 import cc.funkemunky.api.reflections.types.WrappedClass;
+import dev.brighten.api.KauriAPI;
 import dev.brighten.api.data.Data;
+import dev.brighten.db.utils.json.JSONException;
+import lombok.SneakyThrows;
 import org.bukkit.entity.Player;
 
 public class WrappedDataManager extends Wrapper {
 
-    private static WrappedClass objectdataClass = Reflections.getClass("dev.brighten.anticheat.data.ObjectData");
+    private static WrappedClass objectdataClass;
 
-    public WrappedDataManager(Object object) {
-        super(Reflections.getClass("dev.brighten.anticheat.data.DataManager"), object);
+    static {
+        try {
+            objectdataClass = Reflections.getClass(KauriAPI.INSTANCE.object.getString("objectData"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SneakyThrows
+    public WrappedDataManager(WrappedClass wclass, Object object) {
+        super(wclass, object);
     }
 
     public Data getData(Player player) {
-        return fetchMethod("getData", player);
+        return wrappedClass.getMethodByType(objectdataClass.getParent(), 0).invoke(object, player);
     }
 
     public void createData(Player player) {
-        fetchMethod("createData", player);
+        wrappedClass.getMethodByType(Void.class, 0).invoke(object, player);
     }
 }
--- API/src/main/java/dev/brighten/api/wrappers/WrappedKauri.java
+++ API/src/main/java/dev/brighten/api/wrappers/WrappedKauri.java
@@ -1,16 +1,21 @@
 package dev.brighten.api.wrappers;
 
 import cc.funkemunky.api.reflections.Reflections;
+import cc.funkemunky.api.reflections.types.WrappedClass;
+import dev.brighten.api.KauriAPI;
+import lombok.SneakyThrows;
 import org.bukkit.plugin.Plugin;
 
 public class WrappedKauri extends Wrapper {
 
-    public WrappedKauri(Object object) {
-        super(Reflections.getClass("dev.brighten.anticheat.Kauri"), object);
+    public WrappedKauri(WrappedClass wclass, Object object) {
+        super(wclass, object);
     }
 
+    @SneakyThrows
     public WrappedDataManager getDataManager() {
-        return new WrappedDataManager(fetchField("dataManager"));
+        WrappedClass dataMClass = Reflections.getClass(KauriAPI.INSTANCE.object.getString("dataManager"));
+        return new WrappedDataManager(dataMClass, wrappedClass.getFieldByType(dataMClass.getParent(), 0));
     }
 
     public Plugin getPlugin() {
