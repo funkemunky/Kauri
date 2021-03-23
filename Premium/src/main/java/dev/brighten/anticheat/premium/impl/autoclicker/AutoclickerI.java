b051d99f50aebf770bd657ff491dc8f7c140b626
funkemunky <funkemunkybiz@gmail.com>
2021-03-23 14:20:34 -0400

    Fixing autoclicker (I)

--- Premium/src/main/java/dev/brighten/anticheat/premium/impl/autoclicker/AutoclickerI.java
+++ Premium/src/main/java/dev/brighten/anticheat/premium/impl/autoclicker/AutoclickerI.java
@@ -2,6 +2,7 @@ package dev.brighten.anticheat.premium.impl.autoclicker;
 
 import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInArmAnimationPacket;
 import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInFlyingPacket;
+import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInUseEntityPacket;
 import dev.brighten.anticheat.check.api.Cancellable;
 import dev.brighten.anticheat.check.api.Check;
 import dev.brighten.anticheat.check.api.CheckInfo;
@@ -15,15 +16,32 @@ import dev.brighten.api.check.CheckType;
 @Cancellable(cancelType = CancelType.INTERACT)
 public class AutoclickerI extends Check {
 
-    private int arm;
+    private long lastFlying, lastArm;
+    private int buffer;
 
     @Packet
     public void use(WrappedInArmAnimationPacket packet, long now) {
+        long delta = now - lastFlying;
+
+        if(delta < 10 && now - lastArm > 30) {
+            if(++buffer > 6) {
+                vl++;
+                flag("delta=%s buffer=%s", delta, buffer);
+            }
+        }
+
         debug("arm=%s", now);
+        lastArm = now;
+    }
+
+    @Packet
+    public void onUse(WrappedInUseEntityPacket packet, long now) {
+        debug("use=%s", now);
     }
 
     @Packet
     public void flying(WrappedInFlyingPacket packet, long now) {
-        debug("flying=%s", now);
+        if(data.playerInfo.lastTeleportTimer.isPassed(0) && now - lastFlying > 30)
+        lastFlying = now;
     }
 }
