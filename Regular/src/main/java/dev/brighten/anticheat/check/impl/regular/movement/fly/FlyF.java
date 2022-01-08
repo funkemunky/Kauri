f1089a32bf5e1150a482adec38d3c23e17b341d4
funkemunky <funkemunkybiz@gmail.com>
2022-01-08 13:43:04 -0500

    Fixing Fly (F) and Phase false positive
    Fly (F) false positive occurring when just barely making it on a block ,you move 0.593 y up. (aSwitch report).
    
    Fixing phase false positives on fences with bad bounding boxes. This is a fix done inside of Atlas itself.

--- Regular/src/main/java/dev/brighten/anticheat/check/impl/regular/movement/fly/FlyF.java
+++ Regular/src/main/java/dev/brighten/anticheat/check/impl/regular/movement/fly/FlyF.java
@@ -19,7 +19,8 @@ public class FlyF extends Check {
     public void onPacket(WrappedInFlyingPacket packet) {
         if(data.playerInfo.deltaXZ == 0 && data.playerInfo.deltaY == 0) return;
 
-        double max = Math.max(0.5001, (data.playerInfo.lastVelocity.isNotPassed(20)
+        double max = Math.max((data.playerInfo.clientGround && data.playerInfo.serverGround)
+                ? 0.6001 : 0.5001, (data.playerInfo.lastVelocity.isNotPassed(20)
                 ? Math.max(data.playerInfo.velocityY, data.playerInfo.jumpHeight)
                 : data.playerInfo.jumpHeight) + 0.001);
 
