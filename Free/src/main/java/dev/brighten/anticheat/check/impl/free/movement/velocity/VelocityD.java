0892e966840d387253ec25f465ce31c0b0d42294
funkemunky <funkemunkybiz@gmail.com>
2022-04-04 07:07:35 -0400

    Update VelocityD.java

--- Free/src/main/java/dev/brighten/anticheat/check/impl/free/movement/velocity/VelocityD.java
+++ Free/src/main/java/dev/brighten/anticheat/check/impl/free/movement/velocity/VelocityD.java
@@ -2,7 +2,6 @@ package dev.brighten.anticheat.check.impl.free.movement.velocity;
 
 import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInFlyingPacket;
 import cc.funkemunky.api.tinyprotocol.packet.out.WrappedOutVelocityPacket;
-import cc.funkemunky.api.utils.Tuple;
 import dev.brighten.anticheat.check.api.*;
 import dev.brighten.anticheat.utils.timer.Timer;
 import dev.brighten.anticheat.utils.timer.impl.TickTimer;
@@ -10,13 +9,10 @@ import dev.brighten.api.KauriVersion;
 import dev.brighten.api.check.CancelType;
 import dev.brighten.api.check.CheckType;
 import dev.brighten.api.check.DevStage;
-import lombok.val;
+import lombok.Getter;
 import lombok.var;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @CheckInfo(name = "Velocity (D)", description = "Checks if a player responded to velocity",
@@ -25,9 +21,8 @@ import java.util.stream.Collectors;
 public class VelocityD extends Check {
 
     private int buffer;
-    private double vY;
     private Timer lastVelocity = new TickTimer();
-    private Set<Tuple<Double, Long>> velocityY = new HashSet<>();
+    private List<Velocity> velocityY = Collections.synchronizedList(new ArrayList<>());
 
     @Setting(name = "bufferThreshold")
     private static int bufferThreshold = 3;
@@ -36,7 +31,7 @@ public class VelocityD extends Check {
     public void onVelocity(WrappedOutVelocityPacket packet) {
         if(packet.getId() == data.getPlayer().getEntityId() && packet.getY() > 0.1) {
             data.runKeepaliveAction(ka ->
-                    velocityY.add(new Tuple<>(packet.getY(), System.currentTimeMillis())));
+                    velocityY.add(new Velocity(packet.getY())));
         }
     }
 
@@ -45,17 +40,19 @@ public class VelocityD extends Check {
         if(velocityY.size() == 0) return;
 
         var toRemove = velocityY.stream()
-                .filter(t -> Math.abs(data.playerInfo.deltaY - t.one) < 1E-4)
-                .collect(Collectors.toList());
+                .filter(t -> Math.abs(data.playerInfo.deltaY - t.getVelocity()) < 0.001)
+                .iterator();
 
-        //Player took velocity
-        if(toRemove.size() > 0) {
-            toRemove.forEach(velocityY::remove);
-            toRemove.clear();
+        while(toRemove.hasNext()) {
+            Velocity velocity = toRemove.next();
+
             if(buffer > 0) buffer--;
-            debug("Reset velocity: dy=%.4f vy=%.4f", data.playerInfo.deltaY, vY);
-            vY = 0;
-            return;
+            debug("Reset velocity: dy=%.4f vy=%.4f b=%s",
+                    data.playerInfo.deltaY, velocity.getVelocity(), buffer);
+
+            //Removing
+            toRemove.remove();
+
         }
 
         //All potential causes of false positives
@@ -66,24 +63,34 @@ public class VelocityD extends Check {
                 || data.blockInfo.inScaffolding
                 || data.blockInfo.inHoney
                 || data.playerInfo.blockAboveTimer.isNotPassed(2)) {
-            vY = 0;
             debug("Potential false flag");
             return;
         }
 
-        toRemove = velocityY.stream().filter(t -> now - t.two > 500)
-                .collect(Collectors.toList());
+        toRemove = velocityY.stream().filter(t -> now - t.getTimestamp() > 500)
+                .iterator();
 
-        int timeCount = toRemove.size();
-
-        if(timeCount > 0) {
-            toRemove.forEach(velocityY::remove);
-            toRemove.clear();
+        if(toRemove.hasNext()) {
+            List<String> velocities = new ArrayList<>();
+            while(toRemove.hasNext()) {
+                velocities.add(String.valueOf(toRemove.next().getVelocity()));
+                toRemove.remove();
+            }
             if(++buffer > 2) {
                 vl++;
-                flag("lv=%s", lastVelocity.getPassed());
+                flag("lv=%s;v=[%s]", lastVelocity.getPassed(), String.join(";", velocities));
             }
-            vY = 0;
+        }
+    }
+
+    @Getter
+    public static class Velocity {
+        private final double velocity;
+        private final long timestamp;
+
+        public Velocity(double velocityY) {
+            this.velocity = velocityY;
+            this.timestamp = System.currentTimeMillis();
         }
     }
 }
