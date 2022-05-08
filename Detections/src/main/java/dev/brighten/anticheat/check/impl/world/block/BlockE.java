d04a779c0eed4f415778f79881a87d7cc8749541
foofieowo <foofieowo@gmail.com>
2022-05-08 17:29:33 +0200

    renamed sqAccel to accelXZ

--- Detections/src/main/java/dev/brighten/anticheat/check/impl/world/block/BlockE.java
+++ Detections/src/main/java/dev/brighten/anticheat/check/impl/world/block/BlockE.java
@@ -16,19 +16,19 @@ public class BlockE extends Check {
     @Packet
     public void onFlying(WrappedInFlyingPacket packet) {
         final double deltaXZ = data.playerInfo.deltaXZ;
-        final double sqAccel = Math.abs(deltaXZ - data.playerInfo.lDeltaXZ);
+        final double accelXZ = Math.abs(deltaXZ - data.playerInfo.lDeltaXZ);
 
         if (deltaXZ > 0.15D
-                && sqAccel < 1.0E-5
+                && accelXZ < 1.0E-5
                 && (data.playerInfo.deltaYaw > 1 || data.playerInfo.deltaPitch > 1)
                 && data.playerInfo.groundTicks < 18
                 && data.playerInfo.serverGround
                 && data.playerInfo.lastBlockPlace.isNotPassed(15)) {
             vl++;
-            flag("deltaXZ=%s sqAccel=%s", deltaXZ, sqAccel);
+            flag("deltaXZ=%s sqAccel=%s", deltaXZ, accelXZ);
         }
 
-        debug("deltaXZ=%s sqAccel=%s", deltaXZ, sqAccel);
+        debug("deltaXZ=%s sqAccel=%s", deltaXZ, accelXZ);
     }
 
 }
