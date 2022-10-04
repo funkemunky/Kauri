54a69d5beeabd06d9f218012be73997ee637e87f
TalXVI <61031586+TalXVI@users.noreply.github.com>
2022-10-04 19:25:02 -0300

    Add InventoryD

---
+++ Detections/src/main/java/dev/brighten/anticheat/check/impl/movement/inv/InventoryD.java
@@ -0,0 +1,26 @@
+package dev.brighten.anticheat.check.impl.movement.inv;
+
+import cc.funkemunky.api.tinyprotocol.packet.in.WrappedInWindowClickPacket;
+import dev.brighten.anticheat.check.api.Cancellable;
+import dev.brighten.anticheat.check.api.Check;
+import dev.brighten.anticheat.check.api.CheckInfo;
+import dev.brighten.anticheat.check.api.Packet;
+import dev.brighten.api.check.CancelType;
+import dev.brighten.api.check.CheckType;
+import dev.brighten.api.check.DevStage;
+
+@CheckInfo(name = "Inventory (D)", description = "Clicking in inventory while not open.",
+        checkType = CheckType.INVENTORY, devStage = DevStage.ALPHA)
+@Cancellable(cancelType = CancelType.INVENTORY)
+public class InventoryD extends Check {
+
+    @Packet
+    public void onWindow(WrappedInWindowClickPacket packet) {
+        if (data.playerInfo.generalCancel) return;
+
+        if (!data.playerInfo.inventoryOpen) {
+            vl++;
+            flag(80, "id=%s", packet.getId());
+        }
+    }
+}
